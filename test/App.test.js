d1d19b711f11223f2f4c9d5e594d87c0d440e737
kateloschinina <ekaterina.loshchinina@gmail.com>
2017-04-05 21:12:08 +0100

    TDD add form to main page.

--- test/App.test.js
+++ test/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import { shallow, mount } from 'enzyme';
 import ReactDOM from 'react-dom';
 import App from '../src/App';
+import Form from '../src/Form';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('app should contain a form', () => {
+  const wrapper = shallow(<App/>);
+    expect(wrapper.containsAllMatchingElements([
+      <Form />,
+    ])).to.equal(true);
 });
