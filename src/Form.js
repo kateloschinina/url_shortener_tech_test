6e89ff7efee3e87cee77ffb23eec1ab6c5e2d0c8
kateloschinina <ekaterina.loshchinina@gmail.com>
2017-04-06 13:07:46 +0100

    Shorten ULR is saved in database together with long URL.

--- src/Form.js
+++ src/Form.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import database from './helpers/database/Database';
+import { shortener } from './helpers/Shortener';
 
 export class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
       fullURL: '',
-      shortURL: ''
+      dbLength: 0
     };
     this.setText = this.setText.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
   setText(event) {
-    this.setState({fullURL: event.target.value, shortURL: ''});
+    this.setState({fullURL: event.target.value });
   }
   handleClick() {
     const itemsRef = database.ref('/items');
+    database.ref('/items').on('value', snap => {
+      this.state.dbLength = Object.keys(snap.val()).length;
+    });
     itemsRef.push({
       fullURL: this.state.fullURL,
-      shortURL: ''
+      shortURL: 'https://spike.ly/' + shortener(this.state.dbLength)
     }).catch((error) => {
       console.log(error);
     });
